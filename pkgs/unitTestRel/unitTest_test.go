cb80791d7286193d84407506224a782d18ded505
caoxiaoqiang <checao@tencent.com>
2021-05-24 17:04:30 +0800

    unit testing

--- pkgs/unitTestRel/unitTest_test.go
+++ pkgs/unitTestRel/unitTest_test.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"testing"
 )
-
+/*
+1 函数测试定义: func TestXxx(*testing.T)
+2 这个 TestXxx 函数式放在一个文件尾部名 _test.go 中
+*/
 func Test01(t *testing.T) {
 	fmt.Printf("testing ok")
 	t.Log("err.......")
